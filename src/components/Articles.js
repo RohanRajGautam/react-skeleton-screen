4310bb4ea8717be9be2ec838142140a054643ed2
rohan <rohanrajgautam@gmail.com>
2020-10-19 14:02:15 +0545

    articles component

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 const Articles = () => {
+const [posts, setPosts] = useState(null);
+
+useEffect(() => {
+  setTimeout(async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await res.json()
+    setPosts(data);
+  }, 5000);
+});
+
   return (
+      <div className="articles">
+
     <div>
       <h2>Articles</h2>
     </div>
+    {posts && (
+        posts.map(post => (
+            <div className="article" key={post.id}>
+                <h3>{post.title}</h3>
+                <p>{post.body}</p>
+            </div>
+        ))
+    )}
+    {!posts && <div>loading...</div>}
+      </div>
   );
 };
 
