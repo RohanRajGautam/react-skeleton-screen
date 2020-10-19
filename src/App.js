3b68faacf5ae303ce9217c2293356d3836d61cff
rohan <rohanrajgautam@gmail.com>
2020-10-19 13:43:06 +0545

    created article and user component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Articles from './components/Articles';
+import User from './components/User';
 
 function App() {
   return (
@@ -6,6 +8,10 @@ function App() {
       <header>
         <h1>React Skeleton</h1>
       </header>
+      <div className='content'>
+        <Articles />
+        <User />
+      </div>
     </div>
   );
 }
